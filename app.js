731a680c987bf3537c5b44316c7b8bc35c1886b9
Scott Reyell <smreyell@gmail.com>
2019-04-23 14:03:17 -0700

    connected buttons with the display, added two func, displayEndTime and startTimer

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds){
+  // in case there are timers already set
+  clearInterval(countdown);
   const now = Date.now(); // is going to be in milliseconds
   const then = now + seconds * 1000 // we * by 1000 because milliseconds needs to be converted into seconds
+
   displayTimeLeft(seconds);
+
+  // only needs to be run ones
+  displayEndTime(then)
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000)
@@ -26,3 +34,20 @@ function displayTimeLeft(seconds){
   document.title = displayTime;
   timerDisplay.textContent = displayTime;
 }
+
+function displayEndTime(timestamp){
+  const end = new Date(timestamp)
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour -12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0': ''}${minutes} `;
+}
+
+function startTimer(){
+  const seconds = parseInt(this.dataset.time); //must turn into real number with a parseInt
+  timer(seconds);
+
+}
+
+
+buttons.forEach( button => button.addEventListener('click', startTimer));
